ee159dd25fb84e64f7628922a5bb2e379e90a8c7
AleksandrovIgor1 <alexigore@yandex.ru>
2024-01-17 21:21:00 +0500

    add lesson-03 task-03

---
+++ lesson-03/005.js
@@ -0,0 +1,38 @@
+Дан массив users2
+
+const users2 = [
+    {
+        id: 1,
+        user: 'John',
+        surName: 'Doe',
+        address: '123 Main St'
+    },
+    {
+        id: 2,
+        user: 'Jane',
+        surName: 'Smith',
+        address: '456 Elm St'
+    },
+    {
+        id: 3,
+        user: 'Tom',
+        surName: 'Johnson',
+        address: '789 Oak St'
+    },
+];
+    
+📋
+
+
+К массиву users2 добавь новый объект​
+
+const newObj = {
+    id: 4,
+    user: 'Ivan',
+    surName: 'Susanin',
+    address: 'Unknown'
+}
+📋
+
+
+Полученный результат выведи в консоль
