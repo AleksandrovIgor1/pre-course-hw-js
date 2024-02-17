df69806eeb0772d1830d3d710b5a5f8447ade50e
AleksandrovIgor1 <alexigore@yandex.ru>
2024-02-17 21:43:26 +0500

    Delete lesson-05/05 directory

--- lesson-05/05/ui/game/score-block/score-block.js
+++
@@ -1,6 +0,0 @@
-import { data } from '../../../data.js';
-export function ScoreBlock() {
-	const element = document.createElement('div');
-  element.append('Catch: ' + data.catchPoints + '; Miss: ' + data.missPoints);
-	return element;
-}
--- lesson-05/05/ui/game/settings-panel/settings-panel.component.js
+++
@@ -1,7 +0,0 @@
-
-export function SettingsPanel() {
-	const element = document.createElement('div');
-	element.append('Settings will be here');
-	return element;
-}
-
